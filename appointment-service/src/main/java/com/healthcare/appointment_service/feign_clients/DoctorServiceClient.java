1ffedb0f6e5578ef722cc3d44868959f54657d7c
Abdul Sameeu <abdulsameeu26@gmail.com>
2024-08-27 14:12:31 +0530

    Add Eureka Service

--- appointment-service/src/main/java/com/healthcare/appointment_service/feign_clients/DoctorServiceClient.java
+++ appointment-service/src/main/java/com/healthcare/appointment_service/feign_clients/DoctorServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "doctor-service", url = "http://localhost:8200/api/v1/doctors")
+@FeignClient(name = "doctor-service")
 public interface DoctorServiceClient {
 
     // GET /api/v1/doctor/{id}
--- appointment-service/src/main/java/com/healthcare/appointment_service/feign_clients/PatientServiceClient.java
+++ appointment-service/src/main/java/com/healthcare/appointment_service/feign_clients/PatientServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "patient-service", url = "http://localhost:8100/api/v1/patients")
+@FeignClient(name = "patient-service")
 public interface PatientServiceClient {
 
     @GetMapping("/{id}")
